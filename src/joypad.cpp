cd8bf0639277792df7ee84f8d85ba1f21a49df9c
Casey Beach <casey.beach@regence.com>
2017-10-26 16:17:19 -0700

    more hacking

---
+++ src/joypad.cpp
@@ -0,0 +1,33 @@
+namespace Joypad {
+
+
+u8 joypad_bits[2];  // Joypad shift registers.
+bool strobe;        // Joypad strobe latch.
+
+/* Read joypad state (NES register format) */
+u8 read_state(int n)
+{
+    // When strobe is high, it keeps reading A:
+    // Casey: Change this from the gui to some kind of grpc thing
+    //if (strobe)
+    //    return 0x40 | (GUI::get_joypad_state(n) & 1);
+
+    // Get the status of a button and shift the register:
+    u8 j = 0x40 | (joypad_bits[n] & 1);
+    joypad_bits[n] = 0x80 | (joypad_bits[n] >> 1);
+    return j;
+}
+
+void write_strobe(bool v)
+{
+    // Read the joypad data on strobe's transition 1 -> 0.
+    // Casey: Change this from the gui to some kind of grpc thing
+    //if (strobe and !v)
+    //    for (int i = 0; i < 2; i++)
+    //        joypad_bits[i] = GUI::get_joypad_state(i);
+
+    strobe = v;
+}
+
+
+}
